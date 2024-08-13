923c85c61d42560b1c17114a952b34c15e2c0d8e
John Watts <141861086+peterpescadero@users.noreply.github.com>
2024-08-13 15:37:06 -0400

    Update WaitlistReport.sql

--- WaitlistReport.sql
+++ WaitlistReport.sql
@@ -1,16 +1,12 @@
 
---------collect potential member waitlist data
----some are not members so data must be pulled from pre enrolment form answers
-SELECT prg.NAME                      Program,
-       r.patientid                   ID,
-       (SELECT TOP 1 value
-        FROM   cpdataanswers
-        WHERE  preenrollmentformid = pef.id
-               AND questionid = 46)  AS FirstName,
-       (SELECT TOP 1 value
-        FROM   cpdataanswers
-        WHERE  preenrollmentformid = pef.id
-               AND questionid = 47)  AS LastName,
+--collect potential member waitlist data
+--some are not members so data must be pulled from pre enrolment form answers
+--I am utilizing subqueries for this.
+SELECT 
+	prg.NAME Program,
+       	r.patientid ID,
+       (SELECT TOP 1 value FROM cpdataanswers WHERE preenrollmentformid = pef.id AND questionid = 46)  AS FirstName,
+       (SELECT TOP 1 value FROM   cpdataanswers WHERE preenrollmentformid = pef.id AND questionid = 47)  AS LastName,
       
 	   (SELECT TOP 1 value
         FROM   cpdataanswers
