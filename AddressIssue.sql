2acc35f5c082675007d4090aeba7fc7668d056e6
John Watts <141861086+peterpescadero@users.noreply.github.com>
2024-08-13 14:55:30 -0400

    Update AddressIssue.sql

--- AddressIssue.sql
+++ AddressIssue.sql
@@ -1,3 +1,16 @@
---hello
+--Users were sometimes entering two different addresses in Street and Street2 fields. Street2 was intended to be used for additional address information such as apartment or lot number.
+--We believe this was happening when the address needed to be updated. This is the query created to find these examples. 
 SELECT 
-  PatientID, Street, Street2 FROM Registration WHERE Street2 is not null and SiteCode IN (select SiteCode from Associations where Name = 'YMCA of the Triangle') and Street2 not like '%Apt%' and Street2 not like '%Apart%' and Street2 not like '%Trailer%' and Street2 not like '%LOT%' and Street <> Street2
+  PatientID, 
+  Street, 
+  Street2 
+FROM 
+    Registration 
+WHERE 
+  Street2 is not null 
+  and SiteCode IN (select SiteCode from Associations where Name = 'Northern Association') 
+  and Street2 not like '%Apt%' 
+  and Street2 not like '%Apart%' 
+  and Street2 not like '%Trailer%' 
+  and Street2 not like '%LOT%' 
+  and Street <> Street2
